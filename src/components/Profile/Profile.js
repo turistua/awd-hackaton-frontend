4bd8f39aa74217716e995788887c60cce90876a2
Igor <iburyak@gmail.com>
2019-09-13 00:42:19 +0300

    Add footer to profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -77,6 +77,16 @@ export class ProfilePage extends React.PureComponent {
                         </div>
                     </div>
                 </div>
+                <div className={styles.footer}>
+                    <div className={styles.footerBlock}>
+                        <div className={styles.copyright}>© Unilever 2019</div>
+                        <div className={styles.links}>
+                            <a href="#">Cookie policy</a>
+                            <a href="#">Privacy policy</a>
+                            <a href="#">Terms and conditions</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
