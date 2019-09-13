24cf8e01905bae1fd84210fb0178777ac10b05e8
Dmitri Petras <dmitry.petras@dataart.com>
2019-09-13 10:17:46 +0200

    Donate popup layout

--- src/components/Donate/Donate.js
+++ src/components/Donate/Donate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './Donate.module.css';
 
 export class Donate extends React.PureComponent {
 
@@ -28,25 +29,32 @@ export class Donate extends React.PureComponent {
 
     render = () => {
         return (
-            <div>
+            <form className={styles.donate} autoComplete="off">
                 <h2>DONATE</h2>
-                <div>Tokens</div>
-                <div>
-                    <span onClick={() => this.decrement()}>-</span>
-                    <span>{this.state.tokens}</span>
-                    <span onClick={() => this.increment()}>+</span>
+                <div className={styles.plantTokensPreDial}>How many plants do you want to plant</div>
+                <div className={styles.plantTokensDial}>
+                    <div className={styles.plantTokensButton} onClick={() => this.decrement()}>-</div>
+                    <div className={styles.plantTokensNumber}>{this.state.tokens}</div>
+                    <div className={styles.plantTokensButton} onClick={() => this.increment()}>+</div>
                 </div>
-                <div>20 TreeTokens = 1$</div>
-                <form>
-                    <label htmlFor="card">CARD NUMBER</label>
-                    <input name="card" placeholder="XXXX-XXXX-XXXX-XXXX" />
-                    <label htmlFor="expdate">EXP DATE</label>
-                    <input name="expdate" placeholder="MM/YY" />
-                    <label htmlFor="pwd">CVC</label>
-                    <input name="pwd" type="password" placeholder="XXX" />
-                    <button onClick={e => this.onSubmit(e)}>Buy TreeTokens</button>
-                </form>
-            </div>
+                <div className={styles.plantTokensPostDial}>20 TreeTokens = 1$</div>
+                    <fieldset>
+                        <label htmlFor="card">CARD NUMBER</label>
+                        <input name="card" placeholder="XXXX-XXXX-XXXX-XXXX" />
+                        <div>
+                            <div>
+                                <label htmlFor="expdate">EXP DATE</label>
+                                <input name="expdate" placeholder="MM/YY" />
+                            </div>
+                            <div>
+                                <label htmlFor="pwd">CVC</label>
+                                <input name="pwd" type="password" placeholder="XXX" />
+                            </div>
+                        </div>
+                        <div className={styles.hr}></div>
+                        <button onClick={e => this.onSubmit(e)}>Buy TreeTokens</button>
+                    </fieldset>
+            </form>
         );
     };
 }
